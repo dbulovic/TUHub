4fd6fc645a57e77861796e6190948b452c07aa69
David Bulovic <david.bulovic@student.tugraz.at>
2022-02-01 13:33:55 +0100

    fw works and gd initial

--- num_opt/ass4/main.py
+++ num_opt/ass4/main.py
@@ -66,15 +66,15 @@ def task1(signal):
                     alph = 1/((n)**(1/2))
                 else: 
                     alph = (2/n)**(1/2)
-                tmpr.append(alph*np.cos(np.pi/n)*(j)*(i + 1/2))
+                tmpr.append(alph*np.cos((np.pi/n)*(j)*(i + 1/2)))
             Atmp.append(tmpr)
         A = np.array(Atmp)
         return A
 
     def get_b(sigma):
-        ns_signal = [0 for i in range(len(signal))]
+        ns_signal = []
         for i in range(len(signal)):
-            ns_signal[i] = signal[i] + np.random.normal(0, sigma)
+            ns_signal.append(signal[i] + np.random.normal(0, sigma))
 
         b = np.array(ns_signal)
         return b
@@ -82,12 +82,9 @@ def task1(signal):
     # def frst_f(xt:np.ndarray):
     #     return 1/2*((np.linalg.norm(A@xt - b))**2)
 
-    def frank_wolfe(sigma, d, k):
+    def frank_wolfe(d, k, A: np.array, b: np.array):
         xtmp = [1/d for i in range(d)]
         x = np.array(xtmp)
-
-        A = get_A(d)
-        b = get_b(sigma)
 
         for ki in range(k):
             deltaf1 = A.T@(A@x - b)
@@ -104,31 +101,95 @@ def task1(signal):
             t = 2/(ki+1)
 
             x = (1-t)*x+t*y
-        return x, A, b
+        return A@x
 
-    xa, A, b = frank_wolfe(0.01, 15, 1000)
-    res1 = A@xa
-    ax[0,0].plot(signal)
-    ax[0,0].plot(b)
-    ax[0,0].plot(res1)
+    def proj(v: np.array):
+        v = np.sort(v)
+        v = np.flip(v)
+        
+        p = 0
+        for j in range(d):
+            tmp = 0
+            for l in range (j + 1):
+                tmp += v[l]
 
-    xb, A, b = frank_wolfe(0.03, 15, 1000)
-    res2 = A@xb
-    ax[0,1].plot(signal)
-    ax[0,1].plot(b)
-    ax[0,1].plot(res2)
+            if v[j] + (1/(j+1))*(1 - tmp) > 0:
+                p += 1
+        
+        tmp = 0
+        for l in range(p):
+            tmp += v[l]
+        
+        q = (1/p)*(1 - tmp)
 
-    xc, A, b = frank_wolfe(0.01, 100, 10000)
-    res3 = A@xc
-    ax[1,0].plot(signal)
-    ax[1,0].plot(b)
-    ax[1,0].plot(res3)
+        prjtmp = []
+        
+        for j in range(d):
+            prjtmp.append(max(v[j] + q, 0))
 
-    xd, A, b = frank_wolfe(0.01, 5, 1000)
-    res4 = A@xd
-    ax[1,1].plot(signal)
-    ax[1,1].plot(b)
-    ax[1,1].plot(res4)
+        prj = np.array(prjtmp)
+        return prj
+
+    def gd_alg(d, k, A: np.array, b: np.array):
+        L = power_iteration(A.T@A)
+        t = 1.9/L
+
+        xtmp = [1/d for i in range(d)]
+        x = np.array(xtmp)
+
+        for i in range(k):
+            delta_f = A.T@(A@x - b)
+            x = proj(x - t*delta_f)
+
+        return A@x
+
+    d = 15
+    sigma = 0.01
+    A = get_A(d)
+    b = get_b(sigma)
+    res_fw_a = frank_wolfe(d, 1000, A, b)
+    res_gd_a = gd_alg(d, 1000, A, b)
+    ax[0,0].plot(signal, label = 'Clean signal')
+    ax[0,0].plot(b, label = 'Signal + noise')
+    ax[0,0].plot(res_fw_a, '--', label = 'FW clean')
+    ax[0,0].plot(res_gd_a, '--', label="GD clean")
+    ax[0,0].legend()
+
+    d = 15
+    sigma = 0.03
+    A = get_A(d)
+    b = get_b(sigma)
+    res_fw_b = frank_wolfe(d, 1000, A, b)
+    res_gd_b = gd_alg(d, 1000, A, b)
+    ax[0,1].plot(signal, label = 'Clean signal')
+    ax[0,1].plot(b, label = 'Signal + noise')
+    ax[0,1].plot(res_fw_b, '--', label = 'FW clean')
+    ax[0,1].plot(res_gd_b, '--', label="GD clean")
+    ax[0,1].legend()
+
+    d = 100
+    sigma = 0.01
+    A = get_A(d)
+    b = get_b(sigma)
+    res_fw_c = frank_wolfe(d, 1000, A, b)
+    res_gd_c = gd_alg(d, 1000, A, b)
+    ax[1,0].plot(signal, label = 'Clean signal')
+    ax[1,0].plot(b, label = 'Signal + noise')
+    ax[1,0].plot(res_fw_c, '--', label = 'FW clean')
+    ax[1,0].plot(res_gd_c, '--', label="GD clean")
+    ax[1,0].legend()
+
+    d = 5
+    sigma = 0.01
+    A = get_A(d)
+    b = get_b(sigma)
+    res_fw_d = frank_wolfe(d, 1000, A, b)
+    res_gd_d = gd_alg(d, 1000, A, b)
+    ax[1,1].plot(signal, label = 'Clean signal')
+    ax[1,1].plot(b, label = 'Signal + noise')
+    ax[1,1].plot(res_fw_d, '--', label = 'FW clean')
+    ax[1,1].plot(res_gd_d, '--', label="GD clean")
+    ax[1,1].legend()
 
     """ End of your code
     """
